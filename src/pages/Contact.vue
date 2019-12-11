7a95b4ba718f9a29196e1b09bc87777eabb00f38
Bao Tran <tndinhbao@gmail.com>
2019-12-11 14:08:00 +0700

    update

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,56 +2,96 @@
   <Layout>
     <b-container>
       <div>
-        <img class="img-fluid" src="https://res.cloudinary.com/quangdungandgreenpatio/image/upload/c_scale,q_auto,w_1000/v1576037366/posts/Add_a_subheading_l4wrjs.png" />
-        <b-form
-        class="mt-4"
-          name="contact"
-          method="post"
-          @submit.prevent="handleSubmit"
-          action="/success/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:
-              <input name="bot-field" />
-            </label>
-          </p>
-          <b-form-group id="input-group-2" label="Name:" label-for="form-name">
-            <b-form-input
-              id="name"
-              name="name"
-              v-model="form.name"
-              required
-              placeholder="Enter name"
-            />
-          </b-form-group>
+        <img
+          class="img-fluid"
+          src="https://res.cloudinary.com/quangdungandgreenpatio/image/upload/c_scale,q_auto,w_1000/v1576037366/posts/Add_a_subheading_l4wrjs.png"
+        />
+        <b-row class="mt-3">
+          <b-col sm="12" md="4">
+            <GmapMap
+              :center="{lat:14.2939482, lng:109.0897123}"
+              :zoom="15"
+              map-type-id="terrain"
+              style="height: 300px"
+            >
+              <GmapMarker :position="{lat:14.2939482, lng:109.0897123}" />
+            </GmapMap>
+            <dl class="row mt-3">
+              <dt class="col-sm-3">Adress</dt>
+              <dd
+                class="col-sm-9"
+              >Road 632, Duong Lieu Tay Commune, Binh Duong Industrial zone, Binh Duong ward, Phu My town, Binh Dinh province</dd>
 
-          <b-form-group id="input-group-1" label="Email:" label-for="email">
-            <b-form-input
-              id="email"
-              v-model="form.email"
-              name="email"
-              type="email"
-              required
-              placeholder="Enter email"
-            />
-          </b-form-group>
+              <dt class="col-sm-3">Phone</dt>
+              <dd class="col-sm-9">(+84) 256 3758426</dd>
 
-          <b-form-group id="input-group-3" label="Message:" label-for="message">
-            <b-form-textarea
-              id="message"
-              name="message"
-              v-model="form.message"
-              required
-              placeholder="Enter message"
-            />
-          </b-form-group>
+              <dt class="col-sm-3">Fax</dt>
+              <dd class="col-sm-9">(+84) 256 3758427</dd>
 
-          <b-button type="submit" variant="primary">Submit</b-button>
-        </b-form>
+              <dt class="col-sm-3">Email</dt>
+              <dd class="col-sm-9">
+                <dl class="row">
+                  <dd class="col-sm-12">
+                    <a href="mailto:info@quangdungfurniture.com">info@quangdungfurniture.com</a>
+                  </dd>
+                  <dd class="col-sm-12">
+                    <a href="mailto:quangdung.co.ltd.vn@gmail.com">quangdung.co.ltd.vn@gmail.com</a>.
+                  </dd>
+                </dl>
+              </dd>
+            </dl>
+          </b-col>
+          <b-col sm="12" md="8">
+            <b-form
+              name="contact"
+              method="post"
+              @submit.prevent="handleSubmit"
+              action="/success/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input type="hidden" name="name" value="contact" />
+              <p hidden>
+                <label>
+                  Don’t fill this out:
+                  <input name="bot-field" />
+                </label>
+              </p>
+              <b-form-group id="input-group-2" label="Name:" label-for="form-name">
+                <b-form-input
+                  id="name"
+                  name="name"
+                  v-model="form.name"
+                  required
+                  placeholder="Enter name"
+                />
+              </b-form-group>
+
+              <b-form-group id="input-group-1" label="Email:" label-for="email">
+                <b-form-input
+                  id="email"
+                  v-model="form.email"
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Enter email"
+                />
+              </b-form-group>
+
+              <b-form-group id="input-group-3" label="Message:" label-for="message">
+                <b-form-textarea
+                  id="message"
+                  name="message"
+                  v-model="form.message"
+                  required
+                  placeholder="Enter message"
+                />
+              </b-form-group>
+
+              <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+          </b-col>
+        </b-row>
       </div>
     </b-container>
   </Layout>
