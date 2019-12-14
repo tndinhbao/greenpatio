02b500eff18fa7d9b621cf37b9e3ace16d19d1f0
Bao Tran <tndinhbao@gmail.com>
2019-12-14 10:19:27 +0700

    bugfix, vuew producut's images

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,50 +1,50 @@
 <template>
   <Layout>
     <b-container>
-      <div>
-        <img
-          class="img-fluid"
-          src="https://res.cloudinary.com/quangdungandgreenpatio/image/upload/c_scale,q_auto,w_1000/v1576037366/posts/Add_a_subheading_l4wrjs.png"
-        />
-        <b-row>
-          <b-col sm="12" md="6">
-            <dl class="row mt-3">
-              <dt class="col-sm-3">Adress</dt>
-              <dd
-                class="col-sm-9"
-              >Road 632, Duong Lieu Tay Commune, Binh Duong Industrial zone, Binh Duong ward, Phu My town, Binh Dinh province</dd>
+      <img
+        width="100%"
+        class="img-fluid"
+        src="https://res.cloudinary.com/quangdungandgreenpatio/image/upload/c_scale,q_auto,w_1000/v1576037366/posts/Add_a_subheading_l4wrjs.png"
+      />
+      <b-row>
+        <b-col sm="12" md="6">
+          <dl class="row mt-3">
+            <dt class="col-sm-3">Adress</dt>
+            <dd
+              class="col-sm-9"
+            >Road 632, Duong Lieu Tay Commune, Binh Duong Industrial zone, Binh Duong ward, Phu My town, Binh Dinh province</dd>
 
-              <dt class="col-sm-3">Phone</dt>
-              <dd class="col-sm-9">(+84) 256 3758426</dd>
+            <dt class="col-sm-3">Phone</dt>
+            <dd class="col-sm-9">(+84) 256 3758426</dd>
 
-              <dt class="col-sm-3">Fax</dt>
-              <dd class="col-sm-9">(+84) 256 3758427</dd>
+            <dt class="col-sm-3">Fax</dt>
+            <dd class="col-sm-9">(+84) 256 3758427</dd>
 
-              <dt class="col-sm-3">Email</dt>
-              <dd class="col-sm-9">
-                <dl class="row">
-                  <dd class="col-sm-12">
-                    <a href="mailto:info@quangdungfurniture.com">info@quangdungfurniture.com</a>
-                  </dd>
-                  <dd class="col-sm-12">
-                    <a href="mailto:quangdung.co.ltd.vn@gmail.com">quangdung.co.ltd.vn@gmail.com</a>.
-                  </dd>
-                </dl>
-              </dd>
-            </dl>
-          </b-col>
-          <b-col sm="12" md="6">
-             <GmapMap
-              :center="{lat:14.2939482, lng:109.0897123}"
-              :zoom="15"
-              map-type-id="terrain"
-              style="height: 300px"
-              class="mt-3"
-            >
-              <GmapMarker :position="{lat:14.2939482, lng:109.0897123}" />
-            </GmapMap>
-          </b-col>
-          <!-- <b-col sm="12" md="8">
+            <dt class="col-sm-3">Email</dt>
+            <dd class="col-sm-9">
+              <dl class="row">
+                <dd class="col-sm-12">
+                  <a href="mailto:info@quangdungfurniture.com">info@quangdungfurniture.com</a>
+                </dd>
+                <dd class="col-sm-12">
+                  <a href="mailto:quangdung.co.ltd.vn@gmail.com">quangdung.co.ltd.vn@gmail.com</a>.
+                </dd>
+              </dl>
+            </dd>
+          </dl>
+        </b-col>
+        <b-col sm="12" md="6">
+          <GmapMap
+            :center="{lat:14.2939482, lng:109.0897123}"
+            :zoom="15"
+            map-type-id="terrain"
+            style="height: 300px"
+            class="mt-3"
+          >
+            <GmapMarker :position="{lat:14.2939482, lng:109.0897123}" />
+          </GmapMap>
+        </b-col>
+        <!-- <b-col sm="12" md="8">
             <b-form
               name="contact"
               method="post"
@@ -93,9 +93,8 @@
 
               <b-button type="submit" variant="primary">Submit</b-button>
             </b-form>
-          </b-col> -->
-        </b-row>
-      </div>
+        </b-col>-->
+      </b-row>
     </b-container>
   </Layout>
 </template>
--- src/templates/ProductCategory.vue
+++ src/templates/ProductCategory.vue
@@ -5,7 +5,7 @@
       <b-row>
         <b-col xs="2" md="4" v-for="item in $page.products.edges" :key="item.node.id">
           <div class="pb-4">
-            <div class="card border-light h-100">
+            <div class="card border-light h-100" @click="showProduct(item)">
               <img
                 :src="insertImageOptimization({
                 base: $static.metadata.cloudinaryUrl,
@@ -24,6 +24,33 @@
         </b-col>
       </b-row>
     </b-container>
+    <b-modal
+      id="product-modal"
+      :title="displayedProduct && displayedProduct.node.title"
+      centered
+      hide-footer
+      v-show="displayedProduct"
+    >
+      <b-carousel
+        v-if="displayedProduct"
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade
+        indicators
+        controls
+      >
+        <b-carousel-slide
+          class="slide"
+          v-for="slide in displayedProduct.node.images"
+          :key="slide"
+          :img-src="insertImageOptimization({
+                base: $static.metadata.cloudinaryUrl,
+                config: 'q_50,c_scale,h_500',
+                src: slide
+              })"
+        ></b-carousel-slide>
+      </b-carousel>
+    </b-modal>
   </Layout>
 </template>
 
@@ -63,8 +90,17 @@ query {
 <script>
 import { insertImageOptimization } from "../shared/helpers";
 export default {
+  data: function() {
+    return {
+      displayedProduct: null
+    };
+  },
   methods: {
-    insertImageOptimization
+    insertImageOptimization,
+    showProduct(product) {
+      this.$bvModal.show("product-modal");
+      this.displayedProduct = product;
+    }
   },
   created: function() {
     console.log(this);
